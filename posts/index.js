93e8bc5d7950a994d5cd2f0d99fdf712450bec21
Jewel-Naomi <jewelomadi9@gmail.com>
2023-10-06 10:19:38 +0300

    changes
    I attempted to make a web app

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,53 @@
-const express = require('express')
+let form = document.getElementById("form");
+let content = document.getElementById("content");
+let date = document.getElementById("date");
+let title = document.getElementById("title");
+let posts = document.getElementById("posts");
+let msg = document.getElementById("msg");
+let save = document.getElementById("save");
 
-//express app
-const app=express();
+form.addEventListener("save", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    formValidation();
+  });
+  
+  let formValidation = () => {
+    if (content.value === "") {
+        msg.innerHTML = "content cannot be blank";
+        console.log("failure");
+      } else {
+        console.log("successs");
+        msg.innerHTML = "";
+        acceptData();
+      }
+  };
 
-//building a server
-const port = 4000;
+  let data = {};
 
-app.listen(port,()=>{console.log('Listening at port 4000')})
+  let acceptData = () => {
+    data["text"] = content.value;
+  console.log(data);
+  createPost();
+  };
 
-console.log ('Backend programming')
+  let createPost = () => {
+    posts.innerHTML += `<div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fa-regular fa-pen-to-square" style="color: white;"></i>
+      <i onClick="deletePost(this)" class="fa-regular fa-pen-to-square" style="color: white;"></i>
+    </span>
+  </div>`;
+  content.value;
+  };
 
-//CRUD operations
-//create, read, update, delete
+  let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+  };
+
+  let editPost = (e) => {
+    content.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+  };
